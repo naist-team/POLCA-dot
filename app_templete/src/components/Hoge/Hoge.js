52785342a15fe9f47274d0f01ff75ef602d2b26f
藤中秋輔 <keitai54@gmail.com>
2019-05-11 17:45:11 +0900

    refactoring

--- app_templete/src/components/Hoge/Hoge.js
+++ app_templete/src/components/Hoge/Hoge.js
@@ -1,23 +1,23 @@
-import logo from '../../logo.svg';
+import logo from '../../logo.png';
 import React from 'react';
 
 
-export function activeRender(){
-    console.log('Learn react')
-    return(
+export function activeRender() {
+  console.log('Learn react')
+  return (
     <div className="App-header">
-    <img src={logo} className="App-logo" alt="logo" />
-    <p>
-      Edit <code>src/App.js</code> and save to reload.
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
     </p>
-    <a
-      className="App-link"
-      href="https://reactjs.org"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Learn React
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
     </a>
-  </div>
-    )
-  }
+    </div>
+  )
+}
